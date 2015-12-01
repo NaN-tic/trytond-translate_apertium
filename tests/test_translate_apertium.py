133694ff77949115f80701e3daddc5c75a4e0474
resteve <resteve@zikzakmedia.com>
2015-12-01 01:14:14 +0100

    Tests

--- tests/test_translate_apertium.py
+++ tests/test_translate_apertium.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part of translate_apertium module for Tryton.
+# This file is part of the translate_apertium module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TranslateApertiumTestCase(unittest.TestCase):
+class TranslateApertiumTestCase(ModuleTestCase):
     'Test Translate Apertium module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('translate_apertium')
-
-    def test0005views(self):
-        'Test views'
-        test_view('translate')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'translate_apertium'
 
 
 def suite():
